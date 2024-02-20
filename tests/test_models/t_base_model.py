52114dbe14a5bd7f7d07cdda9ecb125c6cad10d7
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-02-20 18:51:22 +0100

    remove an extra line

--- tests/test_models/t_base_model.py
+++ tests/test_models/t_base_model.py
@@ -128,7 +128,7 @@ class TestBaseModel_save(unittest.TestCase):
     def test_save_with_arg(self):
         bm = BaseModel()
         with self.assertRaises(TypeError):
-            bm.save(None)
+            bm.save()
 
     def test_save_updates_file(self):
         bm = BaseModel()
@@ -185,7 +185,7 @@ class TestBaseModel_to_dict(unittest.TestCase):
     def test_to_dict_with_arg(self):
         bm = BaseModel()
         with self.assertRaises(TypeError):
-            bm.to_dict(None)
+            bm.to_dict()
 
 
 if __name__ == "__main__":
